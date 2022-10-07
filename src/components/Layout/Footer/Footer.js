0ba21179978338233ca205f200505d1d47cfa8d4
Kofeinich <agentkonch@mail.ru>
2022-10-07 04:30:06 +0300

    feat: Footer.js added

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -3,7 +3,26 @@ import styles from './Footer.module.css';
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			Footer
+			<div className={styles.left}>
+				<h2 className={styles.heading}>Подписка на мероприятия</h2>
+				<div className={styles.box}>
+					<p className={styles.paragraph}>Ваш e-mail</p>
+					<button className={styles.button}>Подписаться</button>
+				</div>
+			</div>
+			<div className={styles.right}>
+				<div className={styles.list}>
+					<h2 className={styles.heading}>Другие разделы</h2>
+					<p className={styles.paragraph}>Маркетплейс</p>
+					<p className={styles.paragraph}>Достижения</p>
+					<p className={styles.paragraph}>Мероприятия</p>
+				</div>
+				<div className={styles.list}>
+					<h2 className={styles.heading}>Моя коллекция</h2>
+					<p className={styles.paragraph}>Мой профиль</p>
+					<p className={styles.paragraph}>Обменять NFT</p>
+				</div>
+			</div>
 		</footer>
 	)
 }
