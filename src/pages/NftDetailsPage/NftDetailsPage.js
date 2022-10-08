f093d24a52708d04225335c04462d65de5dbf38c
RussianHustla <maximilyn10@mail.ru>
2022-10-08 20:09:10 +0300

    nftDetails init

---
+++ src/pages/NftDetailsPage/NftDetailsPage.js
@@ -0,0 +1,11 @@
+import styles from './NftDetailsPage.module.css'
+import {Layout} from "../../components/Layout/Layout";
+
+export const NftDetailsPage = () => {
+
+	return (
+		<Layout>
+			nft
+		</Layout>
+	)
+}
